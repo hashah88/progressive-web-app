a1b065e1b0013685c04e1fe8e60016d71f3316b2
Hetavi Shah <shahhetvi990@gmail.com>
2019-01-26 17:55:58 -0600

    Gets the data from firebase

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -5,6 +5,8 @@ import { AngularFirestoreDocument,
   AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Task } from "./app.model";
 
+import { Observable } from 'rxjs/internal/Observable';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,17 +15,36 @@ export class FirebaseService {
   
   private tasksCollection: AngularFirestoreCollection<Task>;
   task : Task;
+  tasksList : Observable<Task[]>
 
 
   constructor(public db: AngularFirestore) { 
     // creates the collection
-    this.tasksCollection = db.collection<Task>('Tasks'); 
+    this.tasksCollection = db.collection<Task>('Tasks',ref =>{
+      return ref.orderBy('description');
+    }); 
   }
+
+  getAllTheTasks(): Observable<Task[]>{
+    this.tasksList = this.tasksCollection.valueChanges();
+    return this.tasksList;
+  }
+  //   return this.tasksCollection.get().subscribe(function(querySnapshot){
+  //     querySnapshot.forEach(function(doc) {
+  //       // doc.data() is never undefined for query doc snapshots
+  //       console.log(doc.id, " => ", doc.data());
+  //   });
+  // });
+
+//   return this.
+// }
+
 
   // adds the Task object to the collection
   addTheTask(taskName: string){
     this.task = {
-      description : taskName
+      description : taskName,
+      uniqueId: ""
     }
     this.tasksCollection.add(this.task);
   }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Task} from "../app.model"
 import {FirebaseService} from "../firebase.service"
+import { Observable } from 'rxjs/internal/Observable';
 
 
 
@@ -14,21 +15,23 @@ export class TasksComponent implements OnInit {
 
   constructor(private firebaseService : FirebaseService) { }
 
-  taskList :string[] = [
-    "Wash the dishes", 
-    " Load the clothes in the dryer.",
-    "Fold the clothes up",
-    "Cut down the vegetables",
-    "Call maintenance guy"
-  ];
+  // taskList :string[] = [
+  //   "Wash the dishes", 
+  //   " Load the clothes in the dryer.",
+  //   "Fold the clothes up",
+  //   "Cut down the vegetables",
+  //   "Call maintenance guy"
+  // ];
+
+  taskList : Observable<Task[]>
 
   usersInput:string = "Edit this task"
 
 
   ngOnInit() {
+    this.taskList = this.firebaseService.getAllTheTasks();
   }
 
-  
   addTask(taskName : string ){
     console.log(taskName);
     this.firebaseService.addTheTask(taskName);
