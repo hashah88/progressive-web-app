74ebca06cf9d9755ae50d43fb424d71ee244de8d
Hetavi Shah <shahhetvi990@gmail.com>
2019-01-27 11:30:16 -0600

    gets the document id properly and app works fine now

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,55 +1,51 @@
 import { Injectable } from '@angular/core';
-import { config } from "./app.config"
-import { AngularFirestoreDocument,
+import {
+  AngularFirestoreDocument,
   AngularFirestore,
-  AngularFirestoreCollection } from '@angular/fire/firestore';
+  AngularFirestoreCollection
+} from '@angular/fire/firestore';
 import { Task } from "./app.model";
-
 import { Observable } from 'rxjs/internal/Observable';
+import { map, tap } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
-  
+
   private tasksCollection: AngularFirestoreCollection<Task>;
-  task : Task;
-  tasksList : Observable<Task[]>
+  task: Task;
+  tasksList: Observable<any[]>;
 
 
-  constructor(public db: AngularFirestore) { 
-    // creates the collection
-    this.tasksCollection = db.collection<Task>('Tasks',ref =>{
+  constructor(public db: AngularFirestore) {
+    // The database reference
+    this.tasksCollection = db.collection<Task>('Tasks', ref => {
       return ref.orderBy('description');
-    }); 
+    });
   }
 
-  getAllTheTasks(): Observable<Task[]>{
-    this.tasksList = this.tasksCollection.valueChanges();
+  documentToDomainObject = _ => {
+    const object = _.payload.doc.data();
+    object.id = _.payload.doc.id;
+    return object;
+  }
+
+  // Fetches all the tasks from the database. 
+  getAllTheTasks() {
+    this.tasksList = this.tasksCollection.snapshotChanges()
+      .pipe(map(actions => actions.map(this.documentToDomainObject)));
     return this.tasksList;
   }
-  //   return this.tasksCollection.get().subscribe(function(querySnapshot){
-  //     querySnapshot.forEach(function(doc) {
-  //       // doc.data() is never undefined for query doc snapshots
-  //       console.log(doc.id, " => ", doc.data());
-  //   });
-  // });
 
-//   return this.
-// }
-
-
-  // adds the Task object to the collection
-  addTheTask(taskName: string){
+  // Adss the task as a document in the Database.
+  addTheTask(taskName: string) {
     this.task = {
-      description : taskName,
+      description: taskName,
       uniqueId: ""
     }
     this.tasksCollection.add(this.task);
   }
-
-
-
 }
 
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { Task} from "../app.model"
 import {FirebaseService} from "../firebase.service"
 import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
 
 
 
@@ -14,22 +15,12 @@ import { Observable } from 'rxjs/internal/Observable';
 export class TasksComponent implements OnInit {
 
   constructor(private firebaseService : FirebaseService) { }
-
-  // taskList :string[] = [
-  //   "Wash the dishes", 
-  //   " Load the clothes in the dryer.",
-  //   "Fold the clothes up",
-  //   "Cut down the vegetables",
-  //   "Call maintenance guy"
-  // ];
-
   taskList : Observable<Task[]>
-
   usersInput:string = "Edit this task"
 
-
   ngOnInit() {
-    this.taskList = this.firebaseService.getAllTheTasks();
+    console.log("Hello this is working");
+   this.taskList = this.firebaseService.getAllTheTasks();
   }
 
   addTask(taskName : string ){
